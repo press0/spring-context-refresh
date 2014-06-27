f95588674cc200ec3c12543bdea30e2c618d4178
Clay Press <press0@gmail.com>
2014-06-27 00:23:50 -0500

    First commit

---
+++ src/test/java/foo/SpringAppTests1.java
@@ -0,0 +1,35 @@
+package foo;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-config.xml")
+public class SpringAppTests1 {
+    @Autowired
+    private HelloService helloService;
+
+    @Autowired
+    private String string;
+
+    @Test
+    public void testSayHello() {
+
+        Assert.assertEquals("foo", string);
+        Assert.assertEquals("Hello world!", helloService.sayHello());
+
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:spring-config2.xml");
+        ((ConfigurableApplicationContext) ctx).refresh();
+        string = ctx.getBean(String.class);
+
+        Assert.assertEquals("bar", string);
+
+    }
+}
